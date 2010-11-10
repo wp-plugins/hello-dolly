2b7d0ea488c1f186d34cdcca2f1e83dcaa631438
nacin <nacin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-10 12:55:47 +0000

    Hello Dolly 1.6.
    git-svn-id: https://plugins.svn.wordpress.org/hello-dolly/trunk@310103 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hello.php
+++ hello.php
@@ -1,15 +1,20 @@
 <?php
+/**
+ * @package Hello_Dolly
+ * @version 1.6
+ */
 /*
 Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/#
+Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Matt Mullenweg
-Version: 1.5
+Version: 1.6
 Author URI: http://ma.tt/
 */
 
-// These are the lyrics to Hello Dolly
-$lyrics = "Hello, Dolly
+function hello_dolly_get_lyric() {
+	/** These are the lyrics to Hello Dolly */
+	$lyrics = "Hello, Dolly
 Well, hello, Dolly
 It's so nice to have you back where you belong
 You're lookin' swell, Dolly
@@ -38,37 +43,40 @@ Dolly'll never go away
 Dolly'll never go away
 Dolly'll never go away again";
 
-// Here we split it into lines
-$lyrics = explode("\n", $lyrics);
-// And then randomly choose a line
-$chosen = wptexturize( $lyrics[ mt_rand(0, count($lyrics) - 1) ] );
+	// Here we split it into lines
+	$lyrics = explode( "\n", $lyrics );
+
+	// And then randomly choose a line
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
 
 // This just echoes the chosen line, we'll position it later
 function hello_dolly() {
-	global $chosen;
+	$chosen = hello_dolly_get_lyric();
 	echo "<p id='dolly'>$chosen</p>";
 }
 
-// Now we set that function up to execute when the admin_footer action is called
-add_action('admin_footer', 'hello_dolly');
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'hello_dolly' );
 
 // We need some CSS to position the paragraph
 function dolly_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
 	echo "
 	<style type='text/css'>
 	#dolly {
-		position: absolute;
-		top: 2.3em;
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
 		margin: 0;
-		padding: 0;
-		right: 10px;
-		font-size: 16px;
-		color: #d54e21;
+		font-size: 11px;
 	}
 	</style>
 	";
 }
 
-add_action('admin_head', 'dolly_css');
+add_action( 'admin_head', 'dolly_css' );
 
 ?>
