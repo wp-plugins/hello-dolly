50eaa7bb81ffe2baca48dfee0b3a5c97e0c9dc9b
ikcom <76174793+ikcom@users.noreply.github.com>
2022-05-27 17:52:13 +0300

    Update hello.php
    Update the description

--- hello.php
+++ hello.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Hello Dolly
 Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will see a random lyric from the song <cite>Hello, Dolly!</cite> in the upper right of your admin screen on every page.
 Author: Matt Mullenweg
 Version: 1.6
 Author URI: http://ma.tt/
